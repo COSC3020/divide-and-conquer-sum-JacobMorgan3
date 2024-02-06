644b12321828ce3d3238eec077befe259e4e385d
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-02-06 15:49:01 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,32 +1,22 @@
-let sum = 0;
-
 function divideAndConquerSum(a) 
 {
-    let interval = Math.ceil(a.length / 3);
-    let start = 0;
-    let endFirstThird = interval;
-    let endSecondThird = interval * 2;
-    let end = a.length;
-
-    if(a.length < 2) 
+    if (a.length < 2)
     {
-        if(a.length == 1)
-        {
-            sum = sum + a[0];
-            return sum;
-        }   
+        if (a.length < 1)
+            return 0;
+        else
+            return a[0];
     }
 
-    else 
+    else
     {
-        divideAndConquerSum(a.slice(0, endFirstThird));
-        divideAndConquerSum(a.slice(endFirstThird, endSecondThird));
-        divideAndConquerSum(a.slice(endSecondThird, end));
+        let interval = Math.ceil(a.length / 3);
+        let start = 0;
+        let endFirstThird = interval;
+        let endSecondThird = interval * 2;
+        let end = a.length;
+
+        return divideAndConquerSum(a.slice(0, endFirstThird)) + divideAndConquerSum(a.slice(endFirstThird, endSecondThird)) + divideAndConquerSum(a.slice(endSecondThird, end));
     }
-        
-    return sum;
+
 }
-
-
-var array = [];
-console.log(divideAndConquerSum(array));
